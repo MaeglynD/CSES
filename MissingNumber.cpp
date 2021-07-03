9111bfbfc1b450a50832d2f6817bb73e62f4b457
MaeglynD <maeglyn.davies@gmail.com>
2021-07-03 16:12:55 +0100

    Added MissingNumber.cpp

---
+++ MissingNumber.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#define ll long long int
+
+int main () {
+	ll n, sum = 0;
+
+	cin >> n;
+
+	for (int i=1; i < n; i++) {
+		int a;
+		cin >> a;
+
+		sum+=a;
+	}
+
+	cout << (n * (n + 1) / 2) - sum;
+	
+	return 0;
+}
+
